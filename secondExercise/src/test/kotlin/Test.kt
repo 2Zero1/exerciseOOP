d52b5c4373ee6b1deead6f825555bd647584db13
2Zero1 <Good7219@hotmail.com>
2021-04-29 15:17:39 +0900

    Set testFramework

---
+++ secondExercise/src/test/kotlin/Test.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions;
+
+
+class Test {
+    @Test
+    internal fun test() {
+        Assertions.assertEquals(1,1)
+    }
+}
+
